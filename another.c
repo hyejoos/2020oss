b550431a94c3accde3cf278eded8e7b0cde76c31
hyejoos <74651251+hyejoos@users.noreply.github.com>
2020-11-25 15:01:13 +0900

    덧셈 알고리즘 추가

--- another.c
+++ another.c
@@ -1,1 +1,18 @@
 another file from tutorial
+
+#include <iostream>
+
+int main() {
+  int num;
+  std::cin>>num;
+  
+  int sum = 0;
+  for(int i=1; i<=num; i++)
+    sum += i;
+    
+  std::cout<<sum<<"\n";
+  
+  return 0;
+  
+  }
+  
